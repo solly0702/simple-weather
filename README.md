1fa5d1699acb2ff472e9e4100955f61242b9b148
Sol Lee <solly0702@gmail.com>
2019-06-12 17:32:11 -0700

    remove .env and add readme to setup api keys

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 ## Requires Docker or Node to run
 
+### Setup API Key
+
+```
+This app requires three api keys for location, weather and google map
+Go to ./app/.env to setup your api keys to run this app
+```
+
 ### to run with docker with localhost:80
 
 ```
--- app/src/setupProxy.js
+++ app/src/setupProxy.js
@@ -1,16 +1,6 @@
 const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
-	const routes = ["/api/**"];
-
-	const options = {
-		target: process.env.REACT_APP_LOCATION_PROXY
-		// changeOrigin: true,
-		// logLevel: "debug",
-		// secure: false
-	};
-
-	// app.use(proxy(routes, options));
 	app.use(proxy("/api", { target: "http://battuta.medunes.net" }));
 
 	return app;
